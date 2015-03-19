6680f62aad685b5d8b049bea25fc5c94e2b01f90
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-03-19 19:25:58 +0100

    Add error codes

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -4,17 +4,22 @@ use std::mem;
 use std::ptr;
 
 fn main() {
-    unsafe {
-        let mut appinfos: ffi::GR_APPLICATION_INFO = mem::zeroed();
-        //appInfo.apiVersion = GR_API_VERSION;
-        appinfos.apiVersion = 0x1000;      // FIXME: total guess
+    for v in (0 .. 0xffffusize) {
+        println!("{}", v);
 
-        let mut gpus = [ptr::null() ; ffi::GR_MAX_PHYSICAL_GPUS];
-        let mut gpus_count = 0;
+        unsafe {
+            let mut appinfos: ffi::GR_APPLICATION_INFO = mem::zeroed();
+            //appInfo.apiVersion = GR_API_VERSION;
+            appinfos.apiVersion = v as u32;      // FIXME: total guess
 
-        let result = ffi::grInitAndEnumerateGpus(&appinfos, ptr::null(), &mut gpus_count,
-                                                 gpus.as_mut_ptr());
-        println!("{} {}", result, gpus_count);
+            let mut gpus = [ptr::null() ; ffi::GR_MAX_PHYSICAL_GPUS];
+            let mut gpus_count = 0;
+
+            let result = ffi::grInitAndEnumerateGpus(&appinfos, ptr::null(), &mut gpus_count,
+                                                     gpus.as_mut_ptr());
+
+            println!("{:?} {}", result, gpus_count);
+        }
     }
 
     println!("Hello, world!");
